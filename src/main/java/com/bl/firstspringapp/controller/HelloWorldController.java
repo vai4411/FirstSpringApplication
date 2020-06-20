97dbb6540979f31322f916a8757aae620800d97e
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-06-20 22:37:49 +0530

    ADDED: Display Message Using Post Mapping

--- src/main/java/com/bl/firstspringapp/controller/HelloWorldController.java
+++ src/main/java/com/bl/firstspringapp/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.bl.firstspringapp.controller;
 
+import com.bl.firstspringapp.model.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,5 +20,10 @@ public class HelloWorldController {
     @GetMapping("/param/{name}")
     public String sayHelloParam(@PathVariable String name) {
         return "Hello" + name + "!";
+    }
+
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user) {
+        return "Hello" + user.getFirstName() + " " + user.getLastName() + "!";
     }
 }
