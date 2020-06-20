1ca65b0accb0b3e2f459afed546cd042c82bdb2a
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-06-20 22:12:34 +0530

    ADDED: Display Message Using Get Mapping

--- src/main/java/com/bl/firstspringapp/controller/HelloWorldController.java
+++ src/main/java/com/bl/firstspringapp/controller/HelloWorldController.java
@@ -15,4 +15,9 @@ public class HelloWorldController {
     public String sayHello(@RequestParam(value = "name") String name) {
         return "Hello " + name + "!";
     }
+
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello" + name + "!";
+    }
 }
